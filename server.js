252f8477697ff9c08c928777bfa607bbd4a1653f
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-05-27 15:34:03 -0400

    connected mongoose

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-const express = require(`express`);
-
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
 const PORT = 3000;
+
+// Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+// Mongoose Middleware
+mongoose.connect(`mongodb://localhost:27017/filament`, { useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false });
+mongoose.connection.once('open', () => {
+  console.log('connected to mongo')
+});
 
 // Index
 app.get(`/filament`, (req, res) => {
-    res.send(`Welcome to the Printer Catalog!`);
+    res.render(`index.ejs`);
 });
 
+// New Entry Page
 app.get(`/filament/new`, (req, res) => {
     res.send(`Add new filament here.`);
 });
 
+// Display Page
 app.get(`/filament/:id`, (req, res) => {
     res.send(`Filament details here.`);
 });
