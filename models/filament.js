4d56ab539628d3a8259489aecd4ffcd5d3e507ba
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-05-30 14:11:10 -0400

    added styling to new page and started partials

--- models/filament.js
+++ models/filament.js
@@ -4,8 +4,8 @@ const filamentSchema = new mongoose.Schema({
   type: { type: String, required: true},
   brand: { type: String, required: true},
   color: { type: String, required: true},
-  img: { type: String, required: false},
   stock: { type: String, required: true},
+  img: { type: String, required: false},
   temperature: { type: String, required: false},
   speed: { type: String, required: false},
   link: { type: String, required: false}
